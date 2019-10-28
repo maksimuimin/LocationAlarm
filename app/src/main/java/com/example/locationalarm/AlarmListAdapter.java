1936dbaf51ac06a450b7fc83027d697293769e61
bin-umar <bin-umar@mail.ru>
2019-10-29 00:56:27 +0300

    first try

---
+++ app/src/main/java/com/example/locationalarm/AlarmListAdapter.java
@@ -0,0 +1,24 @@
+package com.example.locationalarm;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AlarmListAdapter  extends RecyclerView.Adapter<AlarmViewHolder> {
+    @NonNull
+    @Override
+    public AlarmViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlarmViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/example/locationalarm/AlarmViewHolder.java
@@ -0,0 +1,13 @@
+package com.example.locationalarm;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AlarmViewHolder extends RecyclerView.ViewHolder {
+
+    public AlarmViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
