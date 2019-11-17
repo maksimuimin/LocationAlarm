d4e80a563b7c52e6daf765115ef4558af66bcfff
MotychenkovLA <motychenkovLA@gmail.com>
2019-11-17 21:41:18 +0300

    map fragment, self location, can't test

--- app/src/main/java/com/example/locationalarm/MainActivity.java
+++ app/src/main/java/com/example/locationalarm/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.locationalarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int GEO_LOC_PERMISSION_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkGeoLocPermission();
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.fragment_container, MapFragment.create())
+                    .commit();
+        }
     }
+
+    private void checkGeoLocPermission() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    getApplicationContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        GEO_LOC_PERMISSION_REQUEST
+                );
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == GEO_LOC_PERMISSION_REQUEST) {
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                //todo выходить из приложения требуя пермишон
+                //или отобрабражать фрагмент с требованием пермишона и кнопкой на перезапрос
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/locationalarm/MapFragment.java
@@ -0,0 +1,102 @@
+package com.example.locationalarm;
+
+import android.app.Activity;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private static final String INDEX = "INDEX";
+
+    private Activity activity;
+    private GoogleMap googleMap;
+
+    public static MapFragment create() {
+        return new MapFragment();
+    }
+
+    public static MapFragment create(int index) {
+        MapFragment res = new MapFragment();
+        Bundle arguments = new Bundle();
+        arguments.putInt(INDEX, index);
+        res.setArguments(arguments);
+        return res;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View res = inflater.inflate(R.layout.fragment_map, container, false);
+
+        this.activity = getActivity();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        if (fragmentManager != null) {
+            SupportMapFragment googleMapFragment =
+                    (SupportMapFragment) fragmentManager.findFragmentById(R.id.google_map);
+            if (googleMapFragment != null) {
+                googleMapFragment.getMapAsync(this);
+            }
+        }
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            int index = arguments.getInt(INDEX, -1);
+            //todo получение энтити из репо по индексу
+        }
+
+        return res;
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        if (activity != null) {
+            //todo если энтити нету - получаем текущее положение
+            LocationServices
+                    .getFusedLocationProviderClient(activity)
+                    .getLastLocation()
+                    .addOnCompleteListener(activity, new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (task.isSuccessful()) {
+                                Location loc = (Location) task.getResult();
+                                if (loc != null) {
+                                    LatLng latLng = new LatLng(loc.getLatitude(), loc.getLongitude());
+                                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                }
+                            }
+                        }
+                    });
+            //todo если энтити есть - вытащить координаты из нее и зумить туда
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        activity = null;
+        googleMap = null;
+    }
+
+}
