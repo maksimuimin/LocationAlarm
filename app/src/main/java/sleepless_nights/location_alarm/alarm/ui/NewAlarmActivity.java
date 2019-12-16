b3934d8a8ad25045595827f270560ba71ce1c99b
bin-umar <bin-umar@mail.ru>
2019-12-16 10:58:02 +0300

    added new logic for name && destination inputs

--- app/src/main/java/sleepless_nights/location_alarm/alarm/ui/NewAlarmActivity.java
+++ app/src/main/java/sleepless_nights/location_alarm/alarm/ui/NewAlarmActivity.java
@@ -1,5 +1,6 @@
 package sleepless_nights.location_alarm.alarm.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
@@ -7,15 +8,21 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.Objects;
 
@@ -26,8 +33,15 @@ public class NewAlarmActivity extends AppCompatActivity {
 
     private LinearLayout layout;
     private BottomSheetBehavior behavior;
-    private TextInputLayout nameInput;
-    private TextInputLayout addressInput;
+
+    private ImageView sheetSwitcher;
+
+    private EditText nameInput;
+    private EditText addressInput;
+
+    private TextView nameHeader;
+    private TextView addressHeader;
+
     private String name = "";
     private String address = "";
 
@@ -48,15 +62,51 @@ public class NewAlarmActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_24);
         }
 
-        toolbar.setNavigationOnClickListener(onNavigationClickListener);
+        toolbar.setNavigationOnClickListener(onNavigationClick);
+
+        sheetSwitcher = findViewById(R.id.sheet_switcher);
+        sheetSwitcher.setOnClickListener(onSwitchClick);
 
         layout = (LinearLayout) findViewById(R.id.bottom_sheet_layout);
         behavior = BottomSheetBehavior.from(layout);
 
         behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+        behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View view, int newState) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetSwitcher.setImageResource(R.drawable.ic_expand_less_40);
+                    return;
+                }
 
-        nameInput = findViewById(R.id.name);
-        addressInput = findViewById(R.id.destination);
+                sheetSwitcher.setImageResource(R.drawable.ic_expand_more_40);
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+
+            }
+        });
+
+        TextInputLayout nameInputLayout = findViewById(R.id.name);
+        TextInputLayout addressInputLayout = findViewById(R.id.address);
+
+        nameInput = (EditText) nameInputLayout.getEditText();
+        addressInput = (EditText) addressInputLayout.getEditText();
+
+        if (nameInput == null || addressInput == null) {
+            Log.wtf("ERROR", "could not find Edit text of name or address");
+            return;
+        }
+
+        nameHeader = findViewById(R.id.name_header);
+        addressHeader = findViewById(R.id.address_header);
+
+        nameHeader.setOnClickListener(view -> headerClickListener(nameInput));
+        addressHeader.setOnClickListener(view -> headerClickListener(addressInput));
+
+        nameInput.setOnKeyListener(onNameInput);
+        addressInput.setOnKeyListener(onAddressInput);
 
         alarmViewModel = ViewModelProviders
                 .of(Objects.requireNonNull(this)) //Shared with MapFragment
@@ -70,19 +120,11 @@ public class NewAlarmActivity extends AppCompatActivity {
         int state = behavior.getState();
 
         if (state == BottomSheetBehavior.STATE_COLLAPSED) {
-            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
         }
 
-        EditText nameInputEditText = nameInput.getEditText();
-        EditText destinationInputEditText = addressInput.getEditText();
-
-        if (nameInputEditText == null || destinationInputEditText == null) {
-            Log.wtf("ERROR", "could not find Edit text of name or address");
-            return;
-        }
-
-        name = nameInputEditText.getText().toString();
-        address = destinationInputEditText.getText().toString();
+        name = nameInput.getText().toString();
+        address = addressInput.getText().toString();
 
         if (name.isEmpty() || address.isEmpty()) {
             Snackbar.make(view, "Please fill required fields: Name and Destination", Snackbar.LENGTH_LONG)
@@ -94,6 +136,54 @@ public class NewAlarmActivity extends AppCompatActivity {
         alarmViewModel.createAlarm(name, address, true, 0, 0,2000);
     };
 
-    private View.OnClickListener onNavigationClickListener = view -> finish();
+    private View.OnClickListener onNavigationClick = view -> finish();
 
+    private View.OnClickListener onSwitchClick = view -> {
+        int state = behavior.getState();
+
+        switch (state) {
+            case BottomSheetBehavior.STATE_COLLAPSED:
+            case BottomSheetBehavior.STATE_EXPANDED: {
+                behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+                break;
+            }
+
+            case BottomSheetBehavior.STATE_HALF_EXPANDED: {
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                break;
+            }
+        }
+    };
+    
+    private View.OnKeyListener onNameInput = (v, keyCode, event) -> {
+        String name = nameInput.getText().toString();
+        nameHeader.setText(name);
+
+        return false;
+    };
+
+    private View.OnKeyListener onAddressInput = (v, keyCode, event) -> {
+        String address = addressInput.getText().toString();
+        addressHeader.setText(address);
+
+        return false;
+    };
+
+    private void headerClickListener (EditText editText) {
+        int state = behavior.getState();
+
+        if (state == BottomSheetBehavior.STATE_COLLAPSED) {
+            behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+        }
+
+        editText.requestFocus();
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        if (imm == null) {
+            Log.wtf("ERROR", "Error happened in getSystemService");
+            return;
+        }
+
+        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+    }
 }
